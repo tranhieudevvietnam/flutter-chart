3b1bbc5021417976220743fee72493e405fe5fde
tranhieudevvietnam <82859719+tranhieudevvietnam@users.noreply.github.com>
2022-12-14 17:44:36 +0700

    .

---
+++ flutter_chart/lib/line_chart/chart_line.dart
@@ -0,0 +1,374 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+
+class ChartModel {
+  final String time;
+  final num value;
+  final Color color;
+
+  ChartModel(
+      {required this.time, required this.value, this.color = Colors.black});
+}
+
+// ignore: must_be_immutable
+class ChartsLine extends StatefulWidget {
+  final List<List<ChartModel>> listData;
+  Color color;
+  Color colorSelect;
+  Function(int index)? onTap;
+
+  TextStyle? style;
+  double? height;
+
+  ChartsLine(
+      {Key? key,
+      required this.listData,
+      this.height,
+      this.style,
+      this.onTap,
+      this.color = Colors.black,
+      this.colorSelect = Colors.grey})
+      : super(key: key);
+
+  @override
+  State<ChartsLine> createState() => _ChartsLineState();
+}
+
+class _ChartsLineState extends State<ChartsLine> {
+  Offset? position;
+
+  @override
+  Widget build(BuildContext context) {
+    // print("posision: ${position}");
+    return Center(
+      child: Listener(
+        onPointerDown: (event) {
+          // print("posision: ${event.position}");
+          setState(() {
+            position = event.position;
+          });
+        },
+        child: CustomPaint(
+          size: Size(MediaQuery.of(context).size.width, widget.height ?? 300),
+          painter: MyCustomPaint(
+              listData: widget.listData,
+              color: widget.color,
+              onTap: widget.onTap,
+              colorSelect: widget.colorSelect,
+              position: position),
+        ),
+      ),
+    );
+  }
+}
+
+class MyCustomPaint extends CustomPainter {
+  List<List<ChartModel>> listData;
+  Color color;
+  TextStyle? style;
+  double? height;
+  Offset? position;
+  Color colorSelect;
+  Function(int index)? onTap;
+
+  MyCustomPaint(
+      {required this.listData,
+      this.height,
+      this.style,
+      this.position,
+      this.onTap,
+      this.colorSelect = Colors.grey,
+      this.color = Colors.black})
+      : super();
+
+  static const double paddingX = 40;
+  static const double paddingY = 10;
+  double jump = 20;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    int length = 0;
+
+    List<ChartModel> listChartDefault = [];
+    double max = 0;
+    for (var item in listData) {
+      var listData2 = item;
+      var max2 = (listData2.reduce((a, b) => a.value > b.value ? a : b))
+          .value
+          .toDouble();
+      max = max2 > max ? max2 : max;
+      length = item.length > length ? item.length : length;
+      item = listData2;
+      listChartDefault =
+          item.length > listChartDefault.length ? item : listChartDefault;
+    }
+
+    jump = max / 5;
+
+    double widthNext = (size.width - paddingX) / length;
+    double heightNext = (size.height / (max / jump)) - paddingY / 1.5;
+
+    ///paint các giá trị hàng cột ( line và text)
+    _initDrawTextLeftColumn(
+        canvas: canvas, size: size, valueNext: heightNext, maxValue: max);
+
+    ///paint các giá trị hàng ngang ( line và text)
+    _initDrawTextLeftRow(
+      canvas: canvas,
+      size: size,
+      listData: listChartDefault,
+      valueNext: widthNext,
+    );
+
+    for (var item in listData) {
+      List<Offset> listOffset = _getListOffsetByListData(
+          listData: item,
+          heightNext: heightNext,
+          size: size,
+          widthNext: widthNext);
+
+      const pointMode = ui.PointMode.points;
+
+      final paintClick = Paint()
+        ..color = colorSelect
+        ..strokeWidth = 2;
+
+      final paint = Paint()
+        ..color = item.first.color
+        ..strokeWidth = 10
+        ..strokeCap = StrokeCap.round;
+
+      for (int iii = 0; iii < listOffset.length; iii++) {
+        var path = Path()
+          ..addOval(Rect.fromCircle(center: listOffset[iii], radius: 4.0));
+        canvas.drawShadow(path, const Color(0xff000000), 5, true);
+        // item[iii].offset = Offset(listOffset[iii].dx, listOffset[iii].dy);
+
+        if (position != null &&
+            (position!.dx >= (listOffset[iii].dx - widthNext / 2) &&
+                position!.dx <= (listOffset[iii].dx + widthNext / 2)) &&
+            onTap != null) {
+          onTap?.call(iii);
+          canvas.drawLine(
+              Offset(listOffset[iii].dx, 0),
+              Offset(listOffset[iii].dx, size.height - paddingY * 2),
+              paintClick);
+        }
+      }
+
+      ///line giữa các position
+      final paint2 = Paint()
+        ..color = item.first.color
+        ..strokeWidth = 3;
+      // final p1 = const Offset(50, 100);
+      // final p2 = const Offset(150, 75);
+
+      for (int i = 0; (i + 1) < listOffset.length; i++) {
+        canvas.drawLine(listOffset[i], listOffset[i + 1], paint2);
+      }
+
+      canvas.drawPoints(pointMode, listOffset, paint);
+      // canvas.save();
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // throw UnimplementedError();
+    return true;
+  }
+
+  _getListOffsetByListData(
+      {required Size size,
+      required List<ChartModel> listData,
+      required double heightNext,
+      required double widthNext}) {
+    List<Offset> listOffset = [];
+    _getOffsetByListData(
+        size: size,
+        listData: listData,
+        heightNext: heightNext,
+        valueNext: widthNext,
+        x: paddingX,
+        index: 0,
+        listOffset: listOffset);
+    // print("xxxx---${listOffset}");
+    return listOffset;
+  }
+
+  _getOffsetByListData(
+      {required Size size,
+      required List<ChartModel> listData,
+      required double heightNext,
+      required double valueNext,
+      required int index,
+      required List<Offset> listOffset,
+      double x = paddingX}) {
+    debugPrint("valueNext2: $valueNext");
+    var dy = heightNext / (jump / listData[index].value.toDouble()) +
+        (paddingY * 1.5);
+    var dy2 = (size.height - paddingY) - dy;
+    listOffset.add(Offset(x, dy2));
+    if (index + 1 < listData.length) {
+      _getOffsetByListData(
+          size: size,
+          listData: listData,
+          heightNext: heightNext,
+          listOffset: listOffset,
+          index: index + 1,
+          valueNext: valueNext,
+          x: x + valueNext);
+    }
+  }
+
+  void _drawDashedLine(Canvas canvas, Size size, double dy, Paint paint,
+      {double? start}) {
+    // Chage to your preferred size
+    const int dashWidth = 1;
+    const int dashSpace = 5;
+
+    // // Start to draw from left size.
+    // // Of course, you can change it to match your requirement.
+    // double startX = 0;
+    // double y = 10;
+    // // Repeat drawing until we reach the right edge.
+    // // In our example, size.with = 300 (from the SizedBox)
+    // while (startX < size.width) {
+    //   // Draw a small line.
+    //   canvas.drawLine(Offset(startX, y), Offset(startX + dashWidth, y), paint);
+    //   // Update the starting X
+    //   startX += dashWidth + dashSpace;
+    // }
+    // Start to draw from left size.
+    // Of course, you can change it to match your requirement.
+    double startX = start ?? 0;
+    double y = dy;
+    // Repeat drawing until we reach the right edge.
+    // In our example, size.with = 300 (from the SizedBox)
+    while (startX < size.width) {
+      // Draw a small line.
+      canvas.drawLine(Offset(startX, y), Offset(startX + dashWidth, y), paint);
+      // Update the starting X
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  // _initDrawDashedLine({
+  //   required Canvas canvas,
+  //   required Size size,
+  //   double y = paddingY,
+  //   required double valueNext,
+  // }) {
+  //   final Paint paint3 = Paint()
+  //     ..color = const Color(0xffE7E7E8)
+  //     ..strokeCap = StrokeCap.square
+  //     ..strokeWidth = 2;
+  //   _drawDashedLine(canvas, size, y, paint3, start: paddingX);
+  //   _initDrawDashedLine(
+  //       canvas: canvas, size: size, y: y + valueNext, valueNext: valueNext);
+  //   // print("xxxx---$y");
+  //   // if (y + valueNext < size.height) {
+  //   //   _initDrawDashedLine(
+  //   //       canvas: canvas, size: size, y: y + valueNext, valueNext: valueNext);
+  //   // }
+  // }
+
+  _initDrawTextLeftColumn({
+    required Canvas canvas,
+    required Size size,
+    double y = 0,
+    int index = 0,
+    required double valueNext,
+    required double maxValue,
+  }) {
+    // print("xxxx: ${index * 20}");
+    if ((maxValue - (index * jump)) >= 0) {
+      final Paint paint3 = Paint()
+        ..color = const Color(0xffE7E7E8)
+        ..strokeCap = StrokeCap.square
+        ..strokeWidth = 1.5;
+      _drawDashedLine(canvas, size, y + paddingY, paint3, start: paddingX);
+      // ignore: prefer_const_constructors
+      final textStyle = TextStyle(
+        color: color,
+        fontSize: 10,
+      );
+
+      final textSpan = TextSpan(
+        text: "${formatNumberThousand((maxValue - (index * jump)).toInt())} đ",
+        style: style ?? textStyle,
+      );
+      final textPainter = TextPainter(
+        text: textSpan,
+        textDirection: TextDirection.ltr,
+      );
+      textPainter.layout(
+        // minWidth: 0,
+        maxWidth: paddingX,
+      );
+      // final xCenter = (size.width - textPainter.width) / 2;
+      // final yCenter = (size.height - textPainter.height) / 2;
+      final offset = Offset(0, y);
+      textPainter.paint(canvas, offset);
+      _initDrawTextLeftColumn(
+          canvas: canvas,
+          size: size,
+          valueNext: valueNext,
+          maxValue: maxValue,
+          index: index + 1,
+          y: y + valueNext);
+    }
+  }
+
+  _initDrawTextLeftRow({
+    required Canvas canvas,
+    required Size size,
+    double x = paddingX,
+    required List<ChartModel> listData,
+    int index = 1,
+    required double valueNext,
+  }) {
+    // ignore: prefer_const_constructors
+    final textStyle = TextStyle(
+      color: color,
+      fontSize: 10,
+    );
+    final textSpan = TextSpan(
+      text: listData[index - 1].time,
+      style: style ?? textStyle,
+    );
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout(
+      // minWidth: 0,
+      maxWidth: paddingX,
+    );
+    // final xCenter = (size.width - textPainter.width) / 2;
+    // final yCenter = (size.height - textPainter.height) / 2;
+    final offset = Offset(x, size.height - paddingY);
+    textPainter.paint(canvas, offset);
+
+    debugPrint("xxxx: $x");
+    debugPrint("valueNext: $valueNext");
+
+    if (index < listData.length) {
+      _initDrawTextLeftRow(
+          canvas: canvas,
+          size: size,
+          listData: listData,
+          valueNext: valueNext,
+          index: index + 1,
+          x: x + valueNext);
+    }
+  }
+}
+
+String formatNumberThousand(int number) {
+  return intl.NumberFormat.compact(
+    locale: "vi",
+  ).format(number);
+}
---
+++ flutter_chart/lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'line_chart/chart_line.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    List<List<ChartModel>> listData = [];
+    listData = [
+      [
+        ChartModel(
+            time: "T1", value: Random().nextInt(100000000), color: Colors.blue),
+        ChartModel(time: "T2", value: Random().nextInt(100000000)),
+        ChartModel(time: "T3", value: Random().nextInt(100000000)),
+        ChartModel(time: "T4", value: Random().nextInt(100000000)),
+        ChartModel(time: "T5", value: Random().nextInt(100000000)),
+        ChartModel(time: "T6", value: Random().nextInt(100000000)),
+        ChartModel(time: "T7", value: Random().nextInt(100000000)),
+        ChartModel(time: "T8", value: Random().nextInt(100000000)),
+        ChartModel(time: "T9", value: Random().nextInt(100000000)),
+        ChartModel(time: "T10", value: Random().nextInt(100000000)),
+        ChartModel(time: "T11", value: Random().nextInt(100000000)),
+        ChartModel(time: "T12", value: Random().nextInt(100000000)),
+      ],
+      [
+        ChartModel(
+            time: "T1", value: Random().nextInt(100000000), color: Colors.red),
+        ChartModel(time: "T2", value: Random().nextInt(100000000)),
+        ChartModel(time: "T3", value: Random().nextInt(100000000)),
+        ChartModel(time: "T4", value: Random().nextInt(100000000)),
+        ChartModel(time: "T5", value: Random().nextInt(100000000)),
+        ChartModel(time: "T6", value: Random().nextInt(100000000)),
+        ChartModel(time: "T7", value: Random().nextInt(100000000)),
+        ChartModel(time: "T8", value: Random().nextInt(100000000)),
+        ChartModel(time: "T9", value: Random().nextInt(100000000)),
+        ChartModel(time: "T10", value: Random().nextInt(100000000)),
+        ChartModel(time: "T11", value: Random().nextInt(100000000)),
+        ChartModel(time: "T12", value: Random().nextInt(100000000)),
+      ],
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ChartsLine(
+              height: 300,
+              colorSelect: Colors.yellow,
+              onTap: (index) {
+                // todayMoney = listData[1][index].value;
+                // tomorrowMoney = listData[0][index].value;
+                // _streamController.sink.add(null);
+              },
+              style: const TextStyle(fontSize: 10),
+              listData: listData,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
